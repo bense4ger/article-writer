1127bc1cf5ef9b55c7604989a99c3e588edad600
Ben Seager <github@benseager.co.uk>
2018-10-17 19:11:11 +0100

    added reader

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"bense4ger/article-writer/config"
+	"bense4ger/article-writer/service"
+	"fmt"
+)
+
+func main() {
+	err := service.Create(config.C.AWSRegion, config.C.Table)
+	if err != nil {
+		fmt.Printf("Error creating service: %s", err.Error())
+	}
+
+}
---
+++ service/reader.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"bense4ger/article-writer/model"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// ReadArticleFromFile reads a Json file and creates and Article model
+func ReadArticleFromFile(filePath string) (*model.Article, error) {
+	if filePath == "" {
+		return nil, fmt.Errorf("ReadArticleFromFile: No file path")
+	}
+
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("ReadArticleFromFile: %s", err.Error())
+	}
+
+	m := &model.Article{}
+	err = json.Unmarshal(b, m)
+	if err != nil {
+		return nil, fmt.Errorf("ReadArticleFromFile: %s", err.Error())
+	}
+
+	return m, nil
+}
