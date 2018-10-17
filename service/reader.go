aa3b6cfdf58cdd22299967b0e2cfbc193ba387ed
Ben Seager <github@benseager.co.uk>
2018-10-17 19:45:42 +0100

    bug fix

--- service/reader.go
+++ service/reader.go
@@ -24,7 +24,7 @@ func ReadArticleFromFile(filePath string) (*model.Article, error) {
 		return nil, fmt.Errorf("ReadArticleFromFile: %s", err.Error())
 	}
 
-	m.SetKey()
+	m.SetID()
 
 	return m, nil
 }
